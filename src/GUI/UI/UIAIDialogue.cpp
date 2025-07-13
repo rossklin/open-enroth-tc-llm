9a0c0dcd59d626cd76dcb48c3e47cba0dcb6a075
Ross Linscott <rossklin@gmail.com>
2025-07-13 15:11:48 +0200

    A testable (?) UIAIDialogue

--- src/GUI/UI/UIAIDialogue.cpp
+++ src/GUI/UI/UIAIDialogue.cpp
@@ -1,14 +1,26 @@
 #include "UIAIDialogue.h"
 
-#include "Engine/Graphics/Renderer/Renderer.h"
+#include <map>
+#include <string>
+
 #include "Engine/AssetsManager.h"
+#include "Engine/Graphics/Renderer/Renderer.h"
+#include "Engine/Party.h"
 #include "GUI/GUIFont.h"
 #include "GUI/GUIWindow.h"
 
 extern GraphicsImage *game_ui_dialogue_background;
 
+static const std::map<PartyAlignment, std::string> dialogueBackgroundResourceByAlignment = {
+    {PartyAlignment::PartyAlignment_Good, "dpanelg"},
+    {PartyAlignment::PartyAlignment_Neutral, "dpaneln"},
+    {PartyAlignment::PartyAlignment_Evil, "dpanele"},
+};
+
 GUIWindow_AIDialogue::GUIWindow_AIDialogue(int npcId) : GUIWindow(WINDOW_Dialogue, {0, 0}, render->GetRenderDimensions()), _npcId(npcId) {
-    // TODO: Implement constructor
+    if (!game_ui_dialogue_background) {
+        game_ui_dialogue_background = assets->getImage_Solid(dialogueBackgroundResourceByAlignment.at(pParty->alignment));
+    }
 }
 
 void GUIWindow_AIDialogue::Update() {
@@ -17,6 +29,9 @@ void GUIWindow_AIDialogue::Update() {
 }
 
 void GUIWindow_AIDialogue::Release() {
-    // TODO: Implement release logic
+    if (game_ui_dialogue_background) {
+        game_ui_dialogue_background->Release();
+        game_ui_dialogue_background = nullptr;
+    }
     GUIWindow::Release();
 }
